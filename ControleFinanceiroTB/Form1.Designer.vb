af3135806be170caa4ed7692c24e9c828f9d5b41
guto mazzi <gutomazzi@gmail.com>
2019-05-08 21:24:21 -0300

    commit

--- ControleFinanceiroTB/Form1.Designer.vb
+++ ControleFinanceiroTB/Form1.Designer.vb
@@ -25,28 +25,28 @@ Partial Class Form1
         Me.Panel1 = New System.Windows.Forms.Panel()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Panel2 = New System.Windows.Forms.Panel()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.Label3 = New System.Windows.Forms.Label()
-        Me.Label4 = New System.Windows.Forms.Label()
+        Me.lblcredito = New System.Windows.Forms.Label()
+        Me.lbldebito = New System.Windows.Forms.Label()
+        Me.lblsaldo = New System.Windows.Forms.Label()
         Me.Panel3 = New System.Windows.Forms.Panel()
         Me.Label5 = New System.Windows.Forms.Label()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.txtdesc = New System.Windows.Forms.TextBox()
         Me.Label6 = New System.Windows.Forms.Label()
-        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.tipo = New System.Windows.Forms.ComboBox()
         Me.Label7 = New System.Windows.Forms.Label()
-        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.txtvalor = New System.Windows.Forms.TextBox()
         Me.Label8 = New System.Windows.Forms.Label()
-        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.cmbcategoria = New System.Windows.Forms.ComboBox()
         Me.Label9 = New System.Windows.Forms.Label()
-        Me.MaskedTextBox1 = New System.Windows.Forms.MaskedTextBox()
-        Me.Button1 = New System.Windows.Forms.Button()
+        Me.txtdata = New System.Windows.Forms.MaskedTextBox()
+        Me.btnadd = New System.Windows.Forms.Button()
         Me.Label10 = New System.Windows.Forms.Label()
-        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
-        Me.Button2 = New System.Windows.Forms.Button()
+        Me.dgvlanc = New System.Windows.Forms.DataGridView()
+        Me.btnremover = New System.Windows.Forms.Button()
         Me.Panel1.SuspendLayout()
         Me.Panel2.SuspendLayout()
         Me.Panel3.SuspendLayout()
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.dgvlanc, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Panel1
@@ -72,66 +72,66 @@ Partial Class Form1
         'Panel2
         '
         Me.Panel2.BackColor = System.Drawing.Color.MediumSlateBlue
-        Me.Panel2.Controls.Add(Me.Label4)
-        Me.Panel2.Controls.Add(Me.Label3)
-        Me.Panel2.Controls.Add(Me.Label2)
+        Me.Panel2.Controls.Add(Me.lblsaldo)
+        Me.Panel2.Controls.Add(Me.lbldebito)
+        Me.Panel2.Controls.Add(Me.lblcredito)
         Me.Panel2.Location = New System.Drawing.Point(2, 82)
         Me.Panel2.Name = "Panel2"
         Me.Panel2.Size = New System.Drawing.Size(686, 60)
         Me.Panel2.TabIndex = 1
         '
-        'Label2
+        'lblcredito
         '
-        Me.Label2.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
-        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.ForeColor = System.Drawing.Color.White
-        Me.Label2.Location = New System.Drawing.Point(24, 12)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(181, 38)
-        Me.Label2.TabIndex = 0
-        Me.Label2.Text = "R$00,00"
-        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.lblcredito.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.lblcredito.Font = New System.Drawing.Font("Microsoft Sans Serif", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblcredito.ForeColor = System.Drawing.Color.White
+        Me.lblcredito.Location = New System.Drawing.Point(24, 12)
+        Me.lblcredito.Name = "lblcredito"
+        Me.lblcredito.Size = New System.Drawing.Size(181, 38)
+        Me.lblcredito.TabIndex = 0
+        Me.lblcredito.Text = "R$00,00"
+        Me.lblcredito.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
-        'Label3
+        'lbldebito
         '
-        Me.Label3.BackColor = System.Drawing.Color.Red
-        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label3.ForeColor = System.Drawing.Color.White
-        Me.Label3.Location = New System.Drawing.Point(250, 12)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(181, 38)
-        Me.Label3.TabIndex = 1
-        Me.Label3.Text = "R$00,00"
-        Me.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.lbldebito.BackColor = System.Drawing.Color.Red
+        Me.lbldebito.Font = New System.Drawing.Font("Microsoft Sans Serif", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbldebito.ForeColor = System.Drawing.Color.White
+        Me.lbldebito.Location = New System.Drawing.Point(250, 12)
+        Me.lbldebito.Name = "lbldebito"
+        Me.lbldebito.Size = New System.Drawing.Size(181, 38)
+        Me.lbldebito.TabIndex = 1
+        Me.lbldebito.Text = "R$00,00"
+        Me.lbldebito.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
-        'Label4
+        'lblsaldo
         '
-        Me.Label4.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer))
-        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label4.ForeColor = System.Drawing.Color.White
-        Me.Label4.Location = New System.Drawing.Point(471, 12)
-        Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(181, 38)
-        Me.Label4.TabIndex = 2
-        Me.Label4.Text = "R$00,00"
-        Me.Label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.lblsaldo.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.lblsaldo.Font = New System.Drawing.Font("Microsoft Sans Serif", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblsaldo.ForeColor = System.Drawing.Color.White
+        Me.lblsaldo.Location = New System.Drawing.Point(471, 12)
+        Me.lblsaldo.Name = "lblsaldo"
+        Me.lblsaldo.Size = New System.Drawing.Size(181, 38)
+        Me.lblsaldo.TabIndex = 2
+        Me.lblsaldo.Text = "R$00,00"
+        Me.lblsaldo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
         'Panel3
         '
         Me.Panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
-        Me.Panel3.Controls.Add(Me.Button2)
-        Me.Panel3.Controls.Add(Me.DataGridView1)
+        Me.Panel3.Controls.Add(Me.btnremover)
+        Me.Panel3.Controls.Add(Me.dgvlanc)
         Me.Panel3.Controls.Add(Me.Label10)
-        Me.Panel3.Controls.Add(Me.Button1)
-        Me.Panel3.Controls.Add(Me.MaskedTextBox1)
+        Me.Panel3.Controls.Add(Me.btnadd)
+        Me.Panel3.Controls.Add(Me.txtdata)
         Me.Panel3.Controls.Add(Me.Label9)
-        Me.Panel3.Controls.Add(Me.ComboBox2)
+        Me.Panel3.Controls.Add(Me.cmbcategoria)
         Me.Panel3.Controls.Add(Me.Label8)
-        Me.Panel3.Controls.Add(Me.TextBox2)
+        Me.Panel3.Controls.Add(Me.txtvalor)
         Me.Panel3.Controls.Add(Me.Label7)
-        Me.Panel3.Controls.Add(Me.ComboBox1)
+        Me.Panel3.Controls.Add(Me.tipo)
         Me.Panel3.Controls.Add(Me.Label6)
-        Me.Panel3.Controls.Add(Me.TextBox1)
+        Me.Panel3.Controls.Add(Me.txtdesc)
         Me.Panel3.Controls.Add(Me.Label5)
         Me.Panel3.Location = New System.Drawing.Point(5, 146)
         Me.Panel3.Name = "Panel3"
@@ -141,119 +141,129 @@ Partial Class Form1
         'Label5
         '
         Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(6, 18)
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(6, 13)
         Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(39, 13)
+        Me.Label5.Size = New System.Drawing.Size(76, 18)
         Me.Label5.TabIndex = 0
-        Me.Label5.Text = "Label5"
+        Me.Label5.Text = "Descrição"
         '
-        'TextBox1
+        'txtdesc
         '
-        Me.TextBox1.Location = New System.Drawing.Point(9, 34)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(275, 20)
-        Me.TextBox1.TabIndex = 1
+        Me.txtdesc.Location = New System.Drawing.Point(9, 34)
+        Me.txtdesc.Name = "txtdesc"
+        Me.txtdesc.Size = New System.Drawing.Size(275, 20)
+        Me.txtdesc.TabIndex = 1
         '
         'Label6
         '
         Me.Label6.AutoSize = True
-        Me.Label6.Location = New System.Drawing.Point(6, 66)
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(6, 62)
         Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(39, 13)
+        Me.Label6.Size = New System.Drawing.Size(37, 18)
         Me.Label6.TabIndex = 2
-        Me.Label6.Text = "Label6"
+        Me.Label6.Text = "Tipo"
         '
-        'ComboBox1
+        'tipo
         '
-        Me.ComboBox1.FormattingEnabled = True
-        Me.ComboBox1.Location = New System.Drawing.Point(9, 82)
-        Me.ComboBox1.Name = "ComboBox1"
-        Me.ComboBox1.Size = New System.Drawing.Size(120, 21)
-        Me.ComboBox1.TabIndex = 3
+        Me.tipo.FormattingEnabled = True
+        Me.tipo.Items.AddRange(New Object() {"CREDITO", "DEBITO"})
+        Me.tipo.Location = New System.Drawing.Point(9, 82)
+        Me.tipo.Name = "tipo"
+        Me.tipo.Size = New System.Drawing.Size(120, 21)
+        Me.tipo.TabIndex = 3
         '
         'Label7
         '
         Me.Label7.AutoSize = True
-        Me.Label7.Location = New System.Drawing.Point(162, 66)
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(162, 64)
         Me.Label7.Name = "Label7"
-        Me.Label7.Size = New System.Drawing.Size(39, 13)
+        Me.Label7.Size = New System.Drawing.Size(75, 18)
         Me.Label7.TabIndex = 4
-        Me.Label7.Text = "Label7"
+        Me.Label7.Text = "Valor (R$)"
         '
-        'TextBox2
+        'txtvalor
         '
-        Me.TextBox2.Location = New System.Drawing.Point(165, 82)
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(119, 20)
-        Me.TextBox2.TabIndex = 5
+        Me.txtvalor.Location = New System.Drawing.Point(165, 82)
+        Me.txtvalor.Name = "txtvalor"
+        Me.txtvalor.Size = New System.Drawing.Size(119, 20)
+        Me.txtvalor.TabIndex = 5
         '
         'Label8
         '
         Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label8.Location = New System.Drawing.Point(6, 106)
         Me.Label8.Name = "Label8"
-        Me.Label8.Size = New System.Drawing.Size(39, 13)
+        Me.Label8.Size = New System.Drawing.Size(72, 18)
         Me.Label8.TabIndex = 6
-        Me.Label8.Text = "Label8"
+        Me.Label8.Text = "Categoria"
         '
-        'ComboBox2
+        'cmbcategoria
         '
-        Me.ComboBox2.FormattingEnabled = True
-        Me.ComboBox2.Location = New System.Drawing.Point(9, 122)
-        Me.ComboBox2.Name = "ComboBox2"
-        Me.ComboBox2.Size = New System.Drawing.Size(121, 21)
-        Me.ComboBox2.TabIndex = 7
+        Me.cmbcategoria.FormattingEnabled = True
+        Me.cmbcategoria.Items.AddRange(New Object() {"SALARIO", "LAZER", "ALIMENTAÇÃO", "SAUDE", "ALUGUEL"})
+        Me.cmbcategoria.Location = New System.Drawing.Point(9, 126)
+        Me.cmbcategoria.Name = "cmbcategoria"
+        Me.cmbcategoria.Size = New System.Drawing.Size(121, 21)
+        Me.cmbcategoria.TabIndex = 7
         '
         'Label9
         '
         Me.Label9.AutoSize = True
+        Me.Label9.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label9.Location = New System.Drawing.Point(165, 106)
         Me.Label9.Name = "Label9"
-        Me.Label9.Size = New System.Drawing.Size(39, 13)
+        Me.Label9.Size = New System.Drawing.Size(39, 18)
         Me.Label9.TabIndex = 8
-        Me.Label9.Text = "Label9"
+        Me.Label9.Text = "Data"
         '
-        'MaskedTextBox1
+        'txtdata
         '
-        Me.MaskedTextBox1.Location = New System.Drawing.Point(165, 123)
-        Me.MaskedTextBox1.Name = "MaskedTextBox1"
-        Me.MaskedTextBox1.Size = New System.Drawing.Size(119, 20)
-        Me.MaskedTextBox1.TabIndex = 9
+        Me.txtdata.Location = New System.Drawing.Point(165, 127)
+        Me.txtdata.Mask = "00/00/0000"
+        Me.txtdata.Name = "txtdata"
+        Me.txtdata.Size = New System.Drawing.Size(119, 20)
+        Me.txtdata.TabIndex = 9
+        Me.txtdata.ValidatingType = GetType(Date)
         '
-        'Button1
+        'btnadd
         '
-        Me.Button1.Location = New System.Drawing.Point(9, 149)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(269, 20)
-        Me.Button1.TabIndex = 10
-        Me.Button1.Text = "Button1"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.btnadd.Location = New System.Drawing.Point(9, 153)
+        Me.btnadd.Name = "btnadd"
+        Me.btnadd.Size = New System.Drawing.Size(269, 20)
+        Me.btnadd.TabIndex = 10
+        Me.btnadd.Text = "ADICIONAR"
+        Me.btnadd.UseVisualStyleBackColor = True
         '
         'Label10
         '
         Me.Label10.AutoSize = True
-        Me.Label10.Location = New System.Drawing.Point(6, 172)
+        Me.Label10.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label10.Location = New System.Drawing.Point(6, 174)
         Me.Label10.Name = "Label10"
-        Me.Label10.Size = New System.Drawing.Size(45, 13)
+        Me.Label10.Size = New System.Drawing.Size(153, 18)
         Me.Label10.TabIndex = 11
-        Me.Label10.Text = "Label10"
+        Me.Label10.Text = "Últimos Lançamentos"
         '
-        'DataGridView1
+        'dgvlanc
         '
-        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Location = New System.Drawing.Point(9, 188)
-        Me.DataGridView1.Name = "DataGridView1"
-        Me.DataGridView1.Size = New System.Drawing.Size(275, 134)
-        Me.DataGridView1.TabIndex = 12
+        Me.dgvlanc.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvlanc.Location = New System.Drawing.Point(9, 192)
+        Me.dgvlanc.Name = "dgvlanc"
+        Me.dgvlanc.Size = New System.Drawing.Size(275, 134)
+        Me.dgvlanc.TabIndex = 12
         '
-        'Button2
+        'btnremover
         '
-        Me.Button2.Location = New System.Drawing.Point(9, 328)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(275, 21)
-        Me.Button2.TabIndex = 13
-        Me.Button2.Text = "Button2"
-        Me.Button2.UseVisualStyleBackColor = True
+        Me.btnremover.Location = New System.Drawing.Point(9, 332)
+        Me.btnremover.Name = "btnremover"
+        Me.btnremover.Size = New System.Drawing.Size(275, 21)
+        Me.btnremover.TabIndex = 13
+        Me.btnremover.Text = "REMOVER"
+        Me.btnremover.UseVisualStyleBackColor = True
         '
         'Form1
         '
@@ -270,7 +280,7 @@ Partial Class Form1
         Me.Panel2.ResumeLayout(False)
         Me.Panel3.ResumeLayout(False)
         Me.Panel3.PerformLayout()
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.dgvlanc, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
@@ -278,22 +288,22 @@ Partial Class Form1
     Friend WithEvents Panel1 As Panel
     Friend WithEvents Label1 As Label
     Friend WithEvents Panel2 As Panel
-    Friend WithEvents Label4 As Label
-    Friend WithEvents Label3 As Label
-    Friend WithEvents Label2 As Label
+    Friend WithEvents lblsaldo As Label
+    Friend WithEvents lbldebito As Label
+    Friend WithEvents lblcredito As Label
     Friend WithEvents Panel3 As Panel
-    Friend WithEvents Button2 As Button
-    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents btnremover As Button
+    Friend WithEvents dgvlanc As DataGridView
     Friend WithEvents Label10 As Label
-    Friend WithEvents Button1 As Button
-    Friend WithEvents MaskedTextBox1 As MaskedTextBox
+    Friend WithEvents btnadd As Button
+    Friend WithEvents txtdata As MaskedTextBox
     Friend WithEvents Label9 As Label
-    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents cmbcategoria As ComboBox
     Friend WithEvents Label8 As Label
-    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents txtvalor As TextBox
     Friend WithEvents Label7 As Label
-    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents tipo As ComboBox
     Friend WithEvents Label6 As Label
-    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents txtdesc As TextBox
     Friend WithEvents Label5 As Label
 End Class
